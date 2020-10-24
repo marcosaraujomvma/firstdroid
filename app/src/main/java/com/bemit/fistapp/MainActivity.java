f926199b8dc1c796004c8222859d923b72b9ba75
Marcos Araújo <marcos.araujo@neopath.com.br>
2020-10-24 20:21:53 -0300

    update main Activity fuctions

--- app/src/main/java/com/bemit/fistapp/MainActivity.java
+++ app/src/main/java/com/bemit/fistapp/MainActivity.java
@@ -3,12 +3,34 @@ package com.bemit.fistapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int quantidade = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void subtrair(View view) {
+        quantidade = quantidade - 1;
+        displayQuantidade(quantidade);
+    }
+
+
+    public void displayQuantidade(int qtdHamburguer){
+        TextView qtdTextview = (TextView) findViewById(R.id.quantidade_tv);
+        qtdTextview.setText("" + qtdHamburguer);
+
+    }
+
+    public void somar(View view) {
+        quantidade = quantidade + 1;
+        displayQuantidade(quantidade);
+
     }
 }
